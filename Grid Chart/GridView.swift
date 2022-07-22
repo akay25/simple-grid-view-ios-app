5b040c883edeec4e98b28736d885ba28d5dc0f4e
Ajay M <ajay@shivy.co.in>
2022-07-23 01:30:09 +0530

    Added grid view

---
+++ Grid Chart/GridView.swift
@@ -0,0 +1,99 @@
+//
+//  GridView.swift
+//  Grid Chart
+//
+//  Created by Ajay M on 22/07/22.
+//
+import UIKit
+
+class GridView: UIView
+{
+    let GRID_SIZE: CGFloat = 160
+    
+    let GRID_COLOR = UIColor.black.cgColor
+    var zoomFactor: CGFloat = 1
+    
+    var hLinesCount = -1
+    var vLinesCount = -1
+    
+    var showGridLines = true
+    var localGridSize: CGFloat = 160
+    
+    private var path = UIBezierPath()
+    var gridLayer: CAShapeLayer?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        localGridSize = GRID_SIZE * zoomFactor
+        hLinesCount = Int(UIScreen.main.bounds.height / localGridSize)
+        vLinesCount = Int(UIScreen.main.bounds.width / localGridSize)
+        print("Lines: H:\(hLinesCount), V:\(vLinesCount)")
+        print("Gridsize: \(localGridSize)")
+        
+        if(showGridLines){
+            drawGrid()
+        }else{
+            removeGrid()
+        }
+    }
+    
+    fileprivate var gridCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    fileprivate func drawGrid()
+    {
+        path = UIBezierPath()
+        path.lineWidth = 5.0
+        
+        // Vertical lines
+        for i in 0...vLinesCount{
+            let xPos = CGFloat(i) * localGridSize
+
+            path.move(to: CGPoint(x: xPos, y: 0))
+            path.addLine(to: CGPoint(x: xPos, y: bounds.height))
+            path.close()
+        }
+        
+        // Horizontal lines
+        for i in 0...hLinesCount{
+            let yPos = CGFloat(i) * localGridSize
+
+            path.move(to: CGPoint(x: 0, y: yPos))
+            path.addLine(to: CGPoint(x: bounds.height, y: yPos))
+        }
+        
+        path.close()
+        
+        if gridLayer != nil {
+            // Remove gridLayer
+            removeGrid()
+        }
+        // Add it as shape
+        gridLayer = CAShapeLayer()
+        gridLayer?.path = path.cgPath
+        gridLayer?.strokeColor = GRID_COLOR
+        self.layer.addSublayer(gridLayer!)
+
+        //Above line will add this grid at the very top of this view.
+
+        // if you want to add this grid below some layer/object you can use method
+        //self.layer.insertSublayer(gridLayer, at: 0)
+        //Give the index number to this method so that it can place this grid at the index
+    }
+
+    func removeGrid() {
+        guard let gLayer = gridLayer else {return}
+        gLayer.removeFromSuperlayer()
+    }
+}
--- Grid Chart/ViewController.swift
+++ Grid Chart/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Custom view variables
+    @IBOutlet weak var viewport: UIViewController!
+    
+    // Custom private variables
+    var gridView: GridView!
+    var zoomFactor: CGFloat = 0.5
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set grid
+        gridView = GridView()
+        gridView.zoomFactor = zoomFactor
+        gridView.frame = self.view.frame
+        self.view.addSubview(gridView)
+        
+        // Set gesture handler
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap))
+        self.view.addGestureRecognizer(gesture)
     }
-
-
+    
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        let touchPoint = sender.location(in: self.view)
+        print("Clicked at: \(touchPoint.x), \(touchPoint.y)")
+    }
 }
 
